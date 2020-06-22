3c173edede46cb4206f288e1452f8b1a2277c893
市川夕貴 <ichikawayuuki@ichikawayuukinoMacBook-Pro.local>
2020-06-23 05:48:13 +0900

    :tada: ウケるボタンのコンポーネントを追加

---
+++ front/components/atoms/UkeruButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button :class="[$style.ukeruButton]">ウケるｗ</button>
+</template>
+
+<script>
+export default {
+  name: 'UkeruButton'
+}
+</script>
+
+<style module>
+.ukeruButton {
+  appearance: none;
+  border: 0;
+  border-radius: 5px;
+  background: #ffe100;
+  font-weight: bold;
+  color: #000;
+  padding: 8px 16px;
+  font-size: 16px;
+  cursor: pointer;
+  position: relative;
+}
+
+.ukeruButton:hover {
+  background: #f5be14;
+}
+
+.ukeruButton:focus {
+  outline: none;
+}
+
+.ukeruButton:active {
+  top: 2px;
+}
+
+</style>
