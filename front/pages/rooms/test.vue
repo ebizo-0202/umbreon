2a7b24dae39c899480d3238dd23acb60887132cb
市川夕貴 <ichikawayuuki@ichikawayuukinoMacBook-Pro.local>
2020-06-23 06:04:11 +0900

    :tada: お試しでウケるボタンを表示

---
+++ front/pages/rooms/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <ukeruButton />
+
+
+</template>
+
+<script>
+import ukeruButton from '~/components/atoms/UkeruButton.vue'
+export default {
+  components: {
+    ukeruButton
+  }
+}
+</script>
